9bdbe5bfaaa7de924fbc41169d5e29975233749a
Lorena <lorena.giddens@outlook.com>
2018-08-13 21:31:44 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -32,7 +32,7 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/posts/:id/edit' do # UPDATE
-    @post = Post.find_by_id(params:id])
+    @post = Post.find_by_id(params[:id])
     erb :edit
   end
 
