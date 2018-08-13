8fa9d9ee24d59afd9fa0719540480810a58b2965
Lorena <lorena.giddens@outlook.com>
2018-08-13 21:35:39 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,6 +9,44 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/' do
+    redirect '/posts'
+  end
 
+  get '/posts/new' do # CREATE
+    erb :new
+  end
+
+  post '/posts' do # CREATE
+    @post = Post.create(name: params[:name], content: params[:content])
+    redirect '/posts'
+  end
+
+  get '/posts' do # READ
+    @posts = Post.all
+    erb :index
+  end
+
+  get '/posts/:id' do # READ
+    @post = Post.find_by_id(params[:id])
+    erb :show
+  end
+
+  get '/posts/:id/edit' do # UPDATE
+    @post = Post.find_by_id(params[:id])
+    erb :edit
+  end
+
+  patch '/posts/:id' do # UPDATE
+    @post = Post.find_by_id(params[:id])
+    @post.name = params[:name]
+    @post.content = params[:content]
+    @post.save
+    erb :show
+  end
+
+  delete '/posts/:id/delete' do # DELETE
+    @post = Post.find_by_id(params[:id])
+    @post.delete
+    erb :delete
   end
 end
