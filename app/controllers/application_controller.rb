1b31b35d47a109accbe0c843fd7786e2fa6c166f
Lorena <lorena.giddens@outlook.com>
2018-08-13 19:51:50 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,7 +17,7 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/posts' do
-    Post.create(name: params[:name], content: params[:content])
+    @post = Post.create(name: params[:name], content: params[:content])
     redirect '/posts'
   end
 
