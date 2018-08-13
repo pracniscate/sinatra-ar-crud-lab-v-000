81b56f51c4963f8a696780dd0bc41ca4be7c7d70
Lorena <lorena.giddens@outlook.com>
2018-08-13 20:37:43 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -36,7 +36,7 @@ class ApplicationController < Sinatra::Base
   end
 
   patch '/posts/:id' do # UPDATE
-    @post = Post.find(params[:id])
+    @post = Post.find_by_id(params[:id])
     @post.name = params[:name]
     @post.content = params[:content]
     @post.save
